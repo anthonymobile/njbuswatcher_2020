2206693ef887f03742abce56fa24de687717bbb5
Anthony Townsend <anthony@bitsandatoms.net>
2018-05-05 14:58:43 -0400

    quick fix indentation

--- api-1.1/api.py
+++ api-1.1/api.py
@@ -11,32 +11,28 @@ from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
 
-def main():
 
-    e = create_engine('mysql+mysqlconnector://buswatcher:njtransit@localhost/bus_position_log')
-    app = Flask(__name__)
-    api = Api(app)
+e = create_engine('mysql+mysqlconnector://buswatcher:njtransit@localhost/bus_position_log')
+app = Flask(__name__)
+api = Api(app)
 
-    class Routes(Resource):
-        def get(self, route_no):
-            conn = e.connect()
-            query = conn.execute("select * from positions where rt='%s' from positions" % route_no)
-            return {'departments': [i[0] for i in query.cursor.fetchall()]} # how to jsonify the results?
+class Routes(Resource):
+    def get(self, route_no):
+        conn = e.connect()
+        query = conn.execute("select * from positions where rt='%s' from positions" % route_no)
+        return {'departments': [i[0] for i in query.cursor.fetchall()]} # how to jsonify the results?
 
-    api.add_resource(Routes, '/route/<string:rt>')
+api.add_resource(Routes, '/route/<string:rt>')
 
-    '''
-    class Departmental_Salary(Resource):
-        def get(self, department_name):
-            conn = e.connect()
-            query = conn.execute("select * from salaries where Department='%s'" % department_name.upper())
-            # Query the result and get cursor.Dumping that data to a JSON is looked by extension
-            result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
-            return result
-            # We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
-   
-    api.add_resource(Departments_Meta, '/departments')
-    '''
+'''
+class Departmental_Salary(Resource):
+    def get(self, department_name):
+        conn = e.connect()
+        query = conn.execute("select * from salaries where Department='%s'" % department_name.upper())
+        # Query the result and get cursor.Dumping that data to a JSON is looked by extension
+        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        return result
+        # We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
 
-if __name__ == '__main__':
-    app.run()
+api.add_resource(Departments_Meta, '/departments')
+'''
