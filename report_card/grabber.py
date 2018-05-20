21fdf4cd2f58da671acd9b388bba9152866cd6e0
Anthony Townsend <anthony@bitsandatoms.net>
2018-05-20 18:06:51 -0400

    WIP

--- report_card/grabber.py
+++ report_card/grabber.py
@@ -1,24 +1,49 @@
 # bustime NJT v 0.1
-# test profile stops
-# 31732 (j sq)  20615 (central and manhattan)   30189 (webster and congress)    21853 (willow 19th)
 
+import sys
 import StopsDB
-from .. import Buses
+import Buses
 import argparse,datetime
 
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('services', nargs='+', help='Services specified as bus stop#,route# separated by comma with no space')
+    parser.add_argument('-s', '--source', dest='source', default='nj', help='source name')
+    parser.add_argument('-r', '--route', dest='route', required=True, help='Route number')
     args = parser.parse_args()
 
-    db = StopsDB.SQLite('data/%s.db' % args.services[0].split(",")[1])
+    db = StopsDB.SQLite('data/%s.db' % args.route)
 
-    for service in args.services:
-        arrivals = Buses.parse_stopprediction_xml(Buses.get_xml_data('nj', 'stop_predictions', stop=service.split(",")[0], route=service.split(",")[1]))
+    #
+    # grab list of stops on this route from NJT API
+    #
+    # use the Route class in Buses.py to parse the http://mybusnow.njtransit.com/bustime/map/getRoutePoints.jsp?route=119 method
+
+    routedata=Buses.parse_route_xml(Buses.get_xml_data(args.source,'routes',route=args.route))
+
+    # now have to extract only the Buses.Stop instances (Stop.st = stop_id)  from the "Path points" attrib into Stoplist
+
+    # print dir(routedata)
+    # print dir(routedata.Stop)
+    # print dir(routedata.paths)
+    stoplist=[]
+    for i in routedata.paths: # just 1 item
+        for items in i.points:
+            if type_instance=Stop:
+                append.stoplist(st)
+
+    sys.exit()
+
+    #
+    # and iterate grabbing all arrivals for all of those stops
+    #
+    for s in stoplist:
+        arrivals = Buses.parse_stopprediction_xml(
+            Buses.get_xml_data('nj', 'stop_predictions', stop=s, route=route))
         print arrivals
         now = datetime.datetime.now()
         db.insert_positions(arrivals, now)
+
 
 if __name__ == "__main__":
     main()
