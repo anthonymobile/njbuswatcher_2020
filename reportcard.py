7307d8ae5ee16b2ba16423aaa95ce6066bd56266
Anthony Townsend <anthony@bitsandatoms.net>
2018-05-28 22:17:21 -0400

    some output from hourly view, not yet right.

--- reportcard.py
+++ reportcard.py
@@ -5,7 +5,7 @@ import StopsDB
 import Buses
 import datetime, sys, sqlite3, argparse
 import pandas as pd
-import random
+import numpy as np
 from flask import Flask, render_template
 
 
@@ -19,21 +19,25 @@ def getArrivalHistory(source,route):
     return render_template('arrivals_history_full.html', history=history)
 
 
+@app.route('/<source>/<route>/hourly')
+def getHourlyHistory(source,route):
+    stoplist = fetch_arrivals(source,route)
+    hourly = render_arrivals_hourly_mean(source,route,stoplist)
+
+    return render_template('arrivals_history_hourly.html', hourly=hourly)
+
+
 # @app.route('/<source>/<route>/current')
 #
 #     delayboard_current(source,route)
 #     return render_template('templates/tk????.html', arrivals=arrivals)
 #
-# @app.route('<source>/<route>/hourly')
-#
-#     delayboard_hourly(source,route)
-#     return render_template('templates/tk????.html', arrivals=arrivals)
+
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--nofetch', action='store_true',  help='Do not fetch new arrival predictions load cached data from local db')
 args = parser.parse_args()
-
 
 
 def db_setup(route):
@@ -63,6 +67,7 @@ def fetch_arrivals(source,route):
 
 
 def render_arrivals_history_full(source,route,stoplist):
+
     (conn,db) = db_setup(route)
     arrival_query = ('SELECT * FROM stop_predictions WHERE (rd = %s AND pt = "APPROACHING") ORDER BY stop_id,timestamp;' % route)
     df = pd.read_sql_query(arrival_query, conn)
@@ -78,7 +83,7 @@ def render_arrivals_history_full(source,route,stoplist):
         # slice the stop
         df_stop = df.loc[df.stop_id == s]
 
-        # compute interval between this bus and next in log (WORKING)
+        # compute interval between this bus and next in log
         df_stop['delta'] = df_stop['timestamp'] - df_stop['timestamp'].shift(1)
 
         # append it
@@ -90,32 +95,61 @@ def render_arrivals_history_full(source,route,stoplist):
             dict_ins['timestamp'] = row['timestamp']
             dict_ins['delta'] = row['delta']
             arrivals_history_full.append(dict_ins)
-            # print dict_ins
 
     return arrivals_history_full
 
 
-# def render_arrivals_history_tk(source,route,stoplist):
+def render_arrivals_hourly_mean(source,route,stoplist):
 
-    # working = [df_stop.stop_id,(df_stop.resample('H').mean('delta'))]
-    # frequency_board_hrly.append([working['stop_id'], working.index.date, working.index.hour, working['delta']])
+    (conn, db) = db_setup(route)
+    arrival_query = (
+                'SELECT * FROM stop_predictions WHERE (rd = %s AND pt = "APPROACHING") ORDER BY stop_id,timestamp;' % route)
+    df = pd.read_sql_query(arrival_query, conn)
 
+    df['timestamp'] = df['timestamp'].str.split('.').str.get(0)
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df = df.set_index('timestamp', drop=False)
 
-    # NEXT = write a summary of the results to a new data file for archiving and only recalc current day or week
+    arrivals_history_hourly = []
 
-    # open(('data/frequency_board_hrly_%s.txt' % route),w)
+    for s in stoplist:
 
+        # slice the stop
+        df_stop = df.loc[df.stop_id == s]
 
-    # B. delays (observed travel time) = compare bus at stop n and stop n+5 using vehicle id and stopid and average across last n vehicles?
-    # C. schedule adherence = compared observed arrival time against GTFS schedule using run w/ TransitLand API call?
+        # compute interval between this bus and next in log
+        df_stop['delta'] = df_stop['timestamp'] - df_stop['timestamp'].shift(1)
 
+        # resample hourly average
 
-# def render_pages:
-#
-#   move some of the page render code here?
-#   or into the template?
-#   first in text, later in graphics
+        # need to convert delta to numeric type first per...
+        # https://stackoverflow.com/questions/44616546/finding-the-mean-and-standard-deviation-of-a-timedelta-object-in-pandas-df
+        stop_id_bak = df_stop['stop_id']
+        df_stop['delta_int'] = df_stop['delta'].values.astype(np.int64)
+        df_stop = df_stop.delta_int.resample('H').mean()
 
+        for index, value in df_stop.items():
+            dict_ins = {}
+            dict_ins['hour_top'] = index
+            dict_ins['stop_id'] = stop_id_bak
+            dict_ins['avg_interval'] = value
+            arrivals_history_hourly.append(dict_ins)
+
+    return arrivals_history_hourly
+
+# TO DO
+
+#   CORE FUNCTIONALITY
+#   1 ui rendering - first in text, later in graphics. here or in template?
+
+#   NEW FEATURES
+#   1   delays (observed travel time) = compare bus at stop n and stop n+5 using vehicle id and stopid and average across last n vehicles?
+#   2   schedule adherence = compared observed arrival time against GTFS schedule using run w/ TransitLand API call?
+
+#   PERFORMANCE
+#   1   remove redundant code to functions and abstract
+#   2   above should write tables out to static files as able, instead of recalc arrivals always (p
+#   3
 
 
 if __name__ == "__main__":
