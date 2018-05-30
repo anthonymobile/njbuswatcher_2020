e2db224b2ce46fa7aa19144f2534978195ca4cd5
Anthony Townsend <anthony@bitsandatoms.net>
2018-05-30 18:37:37 -0400

    WIP2

--- reportcard.py
+++ reportcard.py
@@ -1,71 +1,75 @@
 # bus report card v1.0
 # May-June 2018
 
-import StopsDB
-import Buses
-import datetime, sys, sqlite3, argparse
+
+import argparse
+
 import pandas as pd
 import numpy as np
 from flask import Flask, render_template
 
+from arrival_grabber import fetch_arrivals, db_setup
 
 app = Flask(__name__)
 
-@app.route('/<source>/<route>/history')
-def getArrivalHistory(source,route):
 
-    stoplist = fetch_arrivals(source,route,args.nofetch)
-    history = render_arrivals_history_full(source,route,stoplist)
+@app.route('/<source>/<route>/history')
+def getArrivalHistory(source, route):
+    stoplist = fetch_arrivals(source, route, args.nofetch)
+    history = render_arrivals_history_full(source, route, stoplist)
     return render_template('arrivals_history_full.html', history=history)
 
+
 @app.route('/<source>/<route>/hourly')
-def getHourlyHistory(source,route):
-    stoplist = fetch_arrivals(source,route,args.nofetch)
-    hourly = render_arrivals_hourly_mean(source,route,stoplist)
+def getHourlyHistory(source, route):
+    stoplist = fetch_arrivals(source, route, args.nofetch)
+    hourly = render_arrivals_hourly_mean(source, route, stoplist)
     return render_template('arrivals_history_hourly.html', hourly=hourly)
 
-# @app.route('/<source>/<route>/current')
+# deprecate the nofetch, this script should never fetch data
+# that should be a separately provisioned cron-controlled process
 #
-#     delayboard_current(source,route)
-#     return render_template('templates/tk????.html', arrivals=arrivals)
-#
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--nofetch', action='store_true',  help='Do not fetch new arrival predictions load cached data from local db')
+parser.add_argument('--nofetch', action='store_true',
+                     help='Do not fetch new arrival predictions load cached data from local db')
 args = parser.parse_args()
 
 
-def db_setup(route):
-    db = StopsDB.SQLite('data/%s.db' % route)
-    conn = sqlite3.connect('data/%s.db' % route)
-    return conn,db
+#
+# THIS GOT MOVED TO arrival_grabber.py
+#
+# def db_setup(route):
+#     db = StopsDB.SQLite('data/%s.db' % route)
+#     conn = sqlite3.connect('data/%s.db' % route)
+#     return conn, db
 
 
-def fetch_arrivals(source,route,flag):
-
-    (conn, db) = db_setup(route)
-    stoplist = []
-    if flag is False:
-        routedata = Buses.parse_route_xml(Buses.get_xml_data(source, 'routes', route=route))
-        for i in routedata.paths:
-            for p in i.points:
-                if p.__class__.__name__ == 'Stop':
-                    stoplist.append(p.identity)
-        for s in stoplist:
-            arrivals = Buses.parse_stopprediction_xml(Buses.get_xml_data('nj', 'stop_predictions', stop=s, route=route))
-            # sys.stdout.write('.')
-            now = datetime.datetime.now()
-            db.insert_positions(arrivals, now)
-    elif flag is True:
-        stoplist_query = (
-                'SELECT stop_id FROM stop_predictions WHERE rd = %s GROUP BY stop_id;' % route)
-        stoplist = pd.read_sql_query(stoplist_query, conn)
-
-    return stoplist.stop_id
+#
+# THIS GOT MOVED TO arrival_grabber.py
+#
+# def fetch_arrivals(source, route, flag):
+#     (conn, db) = db_setup(route)
+#     stoplist = []
+#     if flag is False:
+#         routedata = Buses.parse_route_xml(Buses.get_xml_data(source, 'routes', route=route))
+#         for i in routedata.paths:
+#             for p in i.points:
+#                 if p.__class__.__name__ == 'Stop':
+#                     stoplist.append(p.identity)
+#         for s in stoplist:
+#             arrivals = Buses.parse_stopprediction_xml(Buses.get_xml_data('nj', 'stop_predictions', stop=s, route=route))
+#             # sys.stdout.write('.')
+#             now = datetime.datetime.now()
+#             db.insert_positions(arrivals, now)
+#     elif flag is True:
+#         stoplist_query = (
+#                 'SELECT stop_id FROM stop_predictions WHERE rd = %s GROUP BY stop_id;' % route)
+#         stoplist = pd.read_sql_query(stoplist_query, conn)
+#
+#     return stoplist.stop_id
 
 
 def timestamp_fix(data):
-
     data['timestamp'] = data['timestamp'].str.split('.').str.get(0)
     data['timestamp'] = pd.to_datetime(data['timestamp'])
     data = data.set_index('timestamp', drop=False)
@@ -73,10 +77,10 @@ def timestamp_fix(data):
     return data
 
 
-def render_arrivals_history_full(source,route,stoplist):
-
-    (conn,db) = db_setup(route)
-    arrival_query = ('SELECT * FROM stop_predictions WHERE (rd = %s AND pt = "APPROACHING") ORDER BY stop_id,timestamp;' % route)
+def render_arrivals_history_full(source, route, stoplist):
+    (conn, db) = db_setup(route)
+    arrival_query = (
+                'SELECT * FROM stop_predictions WHERE (rd = %s AND pt = "APPROACHING") ORDER BY stop_id,timestamp;' % route)
     df = pd.read_sql_query(arrival_query, conn)
 
     # CUT
@@ -98,8 +102,8 @@ def render_arrivals_history_full(source,route,stoplist):
         # append it
 
         for index, row in df_stop.iterrows():
-            dict_ins={}
-            dict_ins ['stop_id'] =  row['stop_id']
+            dict_ins = {}
+            dict_ins['stop_id'] = row['stop_id']
             dict_ins['v'] = row['v']
             dict_ins['timestamp'] = row['timestamp']
             dict_ins['delta'] = row['delta']
@@ -108,11 +112,10 @@ def render_arrivals_history_full(source,route,stoplist):
     return arrivals_history_full
 
 
-def render_arrivals_hourly_mean(source,route,stoplist):
-
+def render_arrivals_hourly_mean(source, route, stoplist):
     (conn, db) = db_setup(route)
     arrival_query = (
-                'SELECT * FROM stop_predictions WHERE (rd = %s AND pt = "APPROACHING") ORDER BY stop_id,timestamp;' % route)
+            'SELECT * FROM stop_predictions WHERE (rd = %s AND pt = "APPROACHING") ORDER BY stop_id,timestamp;' % route)
     df = pd.read_sql_query(arrival_query, conn)
 
     # CUT
@@ -147,10 +150,12 @@ def render_arrivals_hourly_mean(source,route,stoplist):
 
     return arrivals_history_hourly
 
+
 # TO DO
 
 #   CORE FUNCTIONALITY
 #   1 ui rendering - first in text, later in graphics. here or in template?
+#   2 render a matplotlib histogram for the hourly, and show it in teh webpage the top of the table
 
 #   NEW FEATURES
 #   1   delays (observed travel time) = compare bus at stop n and stop n+5 using vehicle id and stopid and average across last n vehicles?
