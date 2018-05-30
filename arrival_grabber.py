2e8d1d327a22f10db49ccb578677472a0c237f84
Anthony Townsend <anthony@bitsandatoms.net>
2018-05-30 17:36:31 -0400

    WIP

--- arrival_grabber.py
+++ arrival_grabber.py
@@ -4,11 +4,12 @@
 import argparse
 from reportcard import fetch_arrivals
 
+print 'am i running?'
+
 def main():
     parser = argparse.ArgumentParser()
-        parser.add_argument('-s', '--source', dest='source', required=True, default='nj', help='source name')
-        parser.add_argument('-r', '--route', dest='route', required=True, help='route # ')
-
+    parser.add_argument('-s', '--source', dest='source', required=True, default='nj', help='source name')
+    parser.add_argument('-r', '--route', dest='route', required=True, help='route # ')
     args = parser.parse_args()
 
     flag=False
