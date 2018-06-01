e123a50b11dcbe767e9160383192dd2023bf41a0
Anthony Townsend <anthony@bitsandatoms.net>
2018-06-01 15:35:03 -0400

    re-org tree

--- db/db-optimization.sql
+++
@@ -1,7 +0,0 @@
-USE bus_position_log;
-# https://dev.mysql.com/doc/refman/5.5/en/optimize-overview.html
-# https://stackoverflow.com/questions/9715886/how-to-optimize-the-performance-of-a-very-large-mysql-database
-#
-
-# ALTER TABLE positions_new MODIFY COLUMN ar VARCHAR(20);
-# ALTER TABLE positions_new MODIFY COLUMN bid VARCHAR(20);
