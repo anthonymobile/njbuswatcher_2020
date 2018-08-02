41cd10c941315af713408755d88cd5fb6cb7a5c2
Anthony Townsend <anthony@bitsandatoms.net>
2018-08-01 23:25:13 -0400

    first crack at report card class architecture

--- src/reportcard.py
+++ src/reportcard.py
@@ -1,38 +1,41 @@
-# bus reportcard
+# bus reportcard v0.1
 # flask app running on https://host:5000
 #
-#
-# v0.1 single stop routes
-# stats are aggregated for
-
 
 from flask import Flask, render_template
+import lib.ReportCard
 
 app = Flask(__name__)
 
-# ROUTE VIEW
-# /<source>/<route>/history = single stop all arrivals ever
-# /<source>/<route>/daily   =   "      "  all arrivals since midnight
-# /<source>/<route>/now     =   "      "  all arrivals last 120 minutes
 
+# DEVELOPMENT VIEWS
+
+# list all arrivals for a specific source, route, stop, period
 @app.route('/<source>/<route>/<stop>/<period>')
-def getArrivalHistory1Stop(source, route, stop, period):
+def getArrivals(source, route, stop, period):
+    report = ReportCard.StopReport(source, route)
+    return render_template('stop_report.html', stop_report=report.show_arrivals(period,stop))
 
-    import src.lib.ReportCard
-    report = ReportCard.RouteReport(source, route, stop, period)
+# same thing, but for every stop on a route
+# list all arrivals for a specific source, route,period
+@app.route('/<source>/<route>/<period>')
+def getArrivals(source, route, period):
+    report = ReportCard.RouteReport(source, route, period)
     return render_template('route_report.html', route_report=report)
 
-# STOP VIEW
-# /<source>/<route>/<stop>/history = single stop all arrivals ever
-# /<source>/<route>/<stop>/daily   =   "      "  all arrivals since midnight
-# /<source>/<route>/<stop>/now     =   "      "  all arrivals last 120 minutes
 
-@app.route('/<source>/<route>/<stop>/<period>')
-def getArrivalHistory1Stop(source, route, stop, period):
 
-    from ReportCard import *
-    report = ReportCard(source, route, stop, period)
-    return render_template('stop_report.html', stop_report=report)
+# REPORT CARD VIEWS
+# see OmniGraffle for wireframes
+#
+# /<source>/<route>/history = entire line all arrivals ever
+# /<source>/<route>/weekly  =   "      "  all arrivals this weekdays
+# /<source>/<route>/daily   =   "      "  all arrivals today by hour
+#
+# @app.route('/<source>/<route>/<stop>/<period>')
+# def getArrivalHistory1Stop(source, route, stop, period):
+#     report = ReportCard.RouteReport(source, route, stop, period)
+#     return render_template('route_report.html', route_report=report)
 
 
 if __name__ == "__main__":
