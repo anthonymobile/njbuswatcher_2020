bf97abe0d9135b3b5a31d434ae26b7aeb199510e
Anthony Townsend <anthony@bitsandatoms.net>
2018-08-02 22:37:15 -0400

    WIP, working on arrival record identification and display.

--- src/lib/ReportCard.py
+++ src/lib/ReportCard.py
@@ -1,111 +1,101 @@
 import pandas as pd
 import StopsDB,BusRouteLogsDB
 
-# common - UNUSED
-_dict1 = {
-  'nj': 'http://mybusnow.njtransit.com/bustime/map/'
-}
-_dict2 = {
-  'all_buses': 'getBusesForRouteAll.jsp',
-  'routes': 'getRoutePoints.jsp',
-  'pattern_points': 'getPatternPoints.jsp',
-  'stop_predictions': 'getStopPredictions.jsp',
-  'bus_predictions': 'getBusPredictions.jsp',
-  'buses_for_route': 'getBusesForRoute.jsp',
-  'schedules': 'schedules.jsp',
-  'time_and_temp': 'getTimeAndTemp.jsp',
-  'route_directions_xml':  'routeDirectionStopAsXML',
-}
-def _command1(source, func, **kwargs):
-    result = _sources[source] + _api[func]
-    params = ''
-    for k, v in kwargs.items():
-        params = params + k + '=' + str(v) + '&'
-    if params:
-        result += '?' + params[:-1]
-    return result
-def _command2(tree, key, default=''):
-    res = tree.find(key)
-    if res is not None:
-        return res.text
-    return default
-def common_function(source, function, **kwargs):
-    import urllib2
-    data = urllib2.urlopen(_gen_command(source, function, **kwargs)).read()
-    return data
-def another_common_function(source, function, **kwargs):
-    import urllib2
-    data = urllib2.urlopen(_gen_command(source, function, **kwargs)).read()
-    return data
-
-# common - BY ME
 def timestamp_fix(data): # trim the microseconds off the timestamp and convert it to datetime format
-
     data['timestamp'] = data['timestamp'].str.split('.').str.get(0)
-    # data['timestamp'] = pd.to_datetime(data['timestamp'])
-    # data = data.set_index('timestamp', drop=False)
     data = data.set_index(pd.DatetimeIndex(data['timestamp']), drop=False)
-
     return data
 
-class KeyValueData:
-    def __init__(self, **kwargs):
-        self.name = 'KeyValueData'
-        for k, v in kwargs.items():
-            setattr(self, k, v)
 
-    def add_kv(self, key, value):
-        setattr(self, key, value)
+class StopReport: #---------------------------------------------
+    # creates a object with properties that contain all the content that will be
+    # rendered by the template - e.g. lists that will get iterated over into tables for display
 
-    def __repr__(self):
-        line = []
-        for prop, value in vars(self).iteritems():
-            line.append((prop, value))
-        line.sort(key=lambda x: x[0])
-        out_string = ' '.join([k + '=' + str(v) for k, v in line])
-        return self.name + '[%s]' % out_string
+    def __init__(self,route,stop):
+        self.route=route
+        self.stop=stop
+        self.db = StopsDB.MySQL('buses', 'buswatcher', 'njtransit', '127.0.0.1', self.route)
+        self.conn = self.db.conn
+        self.table_name = 'stop_approaches_log_' + self.route
 
-# main classes
-class Report: #---------------------------------------------
-    def __init__(self, source, route, stop, period): # parent class has stuff common to both Route and Stop reports
-        self.source = source
-        self.route = route
-        self.stop = stop
+
+    def get_approaches(self,period):
         self.period = period
-        #self.pagetitle
-        #self.fieldx
+        if period == "daily":
+            approach_query = ('SELECT * FROM %s WHERE (stop_id= %s AND (DATE(`timestamp`) = CURDATE()) ORDER BY stop_id,timestamp;' % (self.table_name,self.stop))
 
-    class StopReport: #---------------------------------------------
-        # creates a object with properties that contain all the content that will be
-        # rendered by the template
-        # e.g. dicts or lists that will get iterated over into tables for display
+        elif period=="weekly":
+            approach_query = ('SELECT * FROM %s WHERE (stop_id= %s AND (YEARWEEK(`timestamp`, 1) = YEARWEEK(CURDATE(), 1))) ORDER BY stop_id,timestamp;' % (self.table_name,self.stop))
 
-        def __init__(self,route):
-            self.route=route
-            self.db = StopsDB.MySQL('buses', 'buswatcher', 'njtransit', '127.0.0.1', self.route)
-            self.conn = self.db.conn
-            self.table_name = 'stop_approaches_log_' + route
+        elif period=="history":
+            approach_query = ('SELECT * FROM %s WHERE stop_id= %s ORDER BY stop_id,timestamp;' % (self.table_name,self.stop))
 
-        def get_approaches(self,stop,period):
+        df = pd.read_sql_query(approach_query, self.conn)
+        df = timestamp_fix(df)
 
-            if period == "daily":
-                approach_query = ('SELECT * FROM %s WHERE (stop_id= %s AND (DATE(`timestamp`) = CURDATE()) ORDER BY stop_id,timestamp;' % (self.table_name,stop))
+        # return raw list of approaches
+        self.approach_results = []
+        for index, row in df.iterrows():
+            dict_ins = {}
+            dict_ins['stop_id'] = row['stop_id']
+            dict_ins['v'] = row['v']
+            dict_ins['timestamp'] = row['timestamp']
+            self.approach_results.append(dict_ins)
+        return
 
-            elif period=="weekly":
-                approach_query = ('SELECT * FROM %s WHERE (stop_id= %s AND (YEARWEEK(`timestamp`, 1) = YEARWEEK(CURDATE(), 1))) ORDER BY stop_id,timestamp;' % (self.table_name,stop))
 
-            elif period=="history":
-                approach_query = ('SELECT * FROM %s WHERE stop_id= %s) ORDER BY stop_id,timestamp;' % (self.table_name,stop))
+    def get_arrivals1(self, period): # method 1: last approach in a contiguous sequence with 'approaching'
 
-            df = pd.read_sql_query(arrival_query, self.conn)
-            df = timestamp_fix(df)
+        self.period = period
+        if period == "daily":
+            arrival_query = ('SELECT * FROM %s WHERE (stop_id= %s AND pt = "APPROACHING" AND (DATE(`timestamp`) = CURDATE()) ORDER BY stop_id,timestamp;' % (self.table_name,self.stop))
 
-            # return raw list of approaches
-            self.approach_results = []
-            for index, row in df.iterrows():
-                dict_ins = {}
-                dict_ins['stop_id'] = row['stop_id']
-                dict_ins['v'] = row['v']
-                dict_ins['timestamp'] = row['timestamp']
-                self.approach_results.append(dict_ins)
-            return
+        elif period=="weekly":
+            arrival_query = ('SELECT * FROM %s WHERE (stop_id= %s AND pt = "APPROACHING" AND (YEARWEEK(`timestamp`, 1) = YEARWEEK(CURDATE(), 1))) ORDER BY stop_id,timestamp;' % (self.table_name,self.stop))
+
+        elif period=="history":
+            arrival_query = ('SELECT * FROM %s WHERE (stop_id= %s AND pt = "APPROACHING") ORDER BY stop_id,timestamp;' % (self.table_name,self.stop))
+
+        df = pd.read_sql_query(arrival_query, self.conn)
+        df = timestamp_fix(df)
+
+        #
+        # process the df to only take the last one from each 'approaching' sequence
+        #
+        #     for s in stoplist:
+        #         df_stop = df.loc[df.stop_id == s]
+        #
+        #         df_stop['delta'] = df_stop['timestamp'] - df_stop['timestamp'].shift(1)
+        #
+        #
+        #         for index, row in df_stop.iterrows():
+        #             dict_ins = {}
+        #             dict_ins['stop_id'] = row['stop_id']
+        #             dict_ins['v'] = row['v']
+        #             dict_ins['timestamp'] = row['timestamp']
+        #             try:
+        #                 dict_ins['delta'] = row['delta'].seconds
+        #             except:
+        #                 dict_ins['delta'] = row['delta']
+        #             arrivals_history_full.append(dict_ins)
+        #
+
+        # return final list of arrivals
+        self.arrivals = []
+        for index, row in df.iterrows():
+            dict_ins = {}
+            # dict_ins['stop_id'] = row['stop_id']
+            # dict_ins['v'] = row['v']
+            # dict_ins['timestamp'] = row['timestamp']
+            self.arrivals.append(dict_ins)
+        return
+
+
+    def get_arrivals2(self, period):
+        # method 2: geolocated bus to stop using route log tables
+        # (e.g. routelog_87) and stop lat/lon from Route.Stop class
+
+        self.db_routelog = BusRouteLogsDB.MySQL('buses', 'buswatcher', 'njtransit', '127.0.0.1', self.route)
+
+        return
+
--- src/reportcard.py
+++ src/reportcard.py
@@ -10,19 +10,30 @@ app = Flask(__name__)
 
 # DEVELOPMENT VIEWS
 
-# list all approaches for a specific source, route, stop, period
-@app.route('/<source>/<route>/<stop>/<period>')
+# 1 list all approaches for a specific source, route, stop, period
+@app.route('/<source>/<route>/<stop>/<period>/approaches')
 def getApproaches(source, route, stop, period):
-    report = rc.Report.StopReport(source, route)
-    return render_template('dev/stop_report.html', stop_report=report.get_approaches(stop,period))
+    approaches = rc.StopReport(route,stop)
+    approaches.get_approaches(period)
+    return render_template('dev/approaches.html', approaches=approaches)
 
-# # same thing, but for every stop on a route
-# # list all arrivals (last approach in a contiguous sequence with 'approaching' (method 1) or geolocated bus to stop (method 2)  for a specific source, route,period
-# @app.route('/<source>/<route>/<period>')
-# def getArrivals(source, route, period):
-#     report = ReportCard.RouteReport(source, route, period)
-#     return render_template('dev/route_report.html', route_report=report)
-#
+# 2 list all arrivals for a specific source, route, stop, period
+# method 1: last approach in a contiguous sequence with 'approaching'
+@app.route('/<source>/<route>/<stop>/<period>/arrivals')
+def getArrivals1(source, route, stop, period):
+    arrivals1 = rc.StopReport(route,stop)
+    arrivals1.get_arrivals1(period)
+    return render_template('dev/arrivals.html', arrivals=arrivals1)
+
+# method 2: geolocated bus to stop using route log tables (e.g. routelog_87) and stop lat/lon from Route.Stop class
+@app.route('/<source>/<route>/<stop>/<period>/arrivals')
+def getArrivals2(source, route, stop, period):
+    arrivals2 = rc.StopReport(route,stop)
+    arrivals2.get_arrivals2(period)
+    return render_template('dev/arrivals.html', arrivals=arrivals2)
+
+
+
 
 
 # REPORT CARD VIEWS
@@ -36,6 +47,8 @@ def getApproaches(source, route, stop, period):
 # def getArrivalHistory1Stop(source, route, stop, period):
 #     report = ReportCard.RouteReport(source, route, stop, period)
 #     return render_template('route_report.html', route_report=report)
+
+
 
 
 if __name__ == "__main__":
