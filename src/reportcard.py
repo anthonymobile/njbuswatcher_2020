bcfe6cd96cf40c6f35e4ff471db796996dc0a5f7
Anthony Townsend <anthony@bitsandatoms.net>
2018-08-10 21:39:48 -0400

    fixed a bunch of view items: col headings, time formats
    Former-commit-id: 191cabfb63427d2ba8ea639e7b63fa8cc554359e

--- src/reportcard.py
+++ src/reportcard.py
@@ -1,9 +1,10 @@
 # bus reportcard v0.1
 # flask app running on https://host:5000
-#
+
 
 from flask import Flask, render_template
 import lib.ReportCard as rc
+import dateutil
 
 app = Flask(__name__)
 
@@ -33,9 +34,20 @@ def getDelta(source, route, stop, period):
     return render_template('dev/delta.html', arrivals=arrivals)
 
 
+# custom filters
+
+@app.template_filter('strftime_custom')
+def _jinja2_filter_datetime(timestamp, format='%Y %a %b %d %I:%M %p'):
+    return timestamp.strftime(format)
+
+
+
 
 # PRODUCTION VIEWS
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
+
+
+
