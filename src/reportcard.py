f7c936c558eee4b23e40e2309550e243660eb425
Anthony Townsend <anthony@bitsandatoms.net>
2018-08-14 23:45:15 -0400

    big update to UX

--- src/reportcard.py
+++ src/reportcard.py
@@ -1,12 +1,10 @@
 # bus reportcard v0.1
 # flask app running on https://host:5000
 
-
 from flask import Flask, render_template
 import lib.ReportCard as rc
 
 app = Flask(__name__)
-
 
 # DEVELOPMENT VIEWS
 
@@ -22,30 +20,24 @@ def getApproaches(source, route, stop, period):
 def getArrivals(source, route, stop, period):
     arrivals = rc.StopReport(route,stop)
     arrivals.get_arrivals(period)
-    stoplist=rc.get_stoplist(source,route)
-    return render_template('dev/arrivals.html', arrivals=arrivals, stoplist=stoplist)
+    route_stop_list=rc.get_stoplist(source,route)
+    return render_template('dev/arrivals.html', arrivals=arrivals, route_stop_list=route_stop_list)
 
-
-
-# 3 simple arrival list with delta
-@app.route('/<source>/<route>/<stop>/<period>/delta')
-def getDelta(source, route, stop, period):
-    arrivals = rc.StopReport(route,stop)
-    arrivals.get_arrivals(period)
-    arrivals.delta_list()
-    return render_template('dev/delta.html', arrivals=arrivals)
+# # 3 simple arrival list with delta
+# @app.route('/<source>/<route>/<stop>/<period>/delta')
+# def getDelta(source, route, stop, period):
+#     arrivals = rc.StopReport(route,stop)
+#     arrivals.get_arrivals(period)
+#     arrivals.delta_list()
+#     return render_template('dev/delta.html', arrivals=arrivals)
 
 
 # custom filters
 
 @app.template_filter('strftime_custom')
-def _jinja2_filter_datetime(timestamp, format='%Y %a %b %d %I:%M %p'):
+def _jinja2_filter_datetime(timestamp, format='%a %b %d %I:%M %p'):
     return timestamp.strftime(format)
 
-
-
-
-# PRODUCTION VIEWS
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
