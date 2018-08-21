7c04741a577085acfe4041b2a3e0b1b71772f143
Anthony Townsend <anthony@bitsandatoms.net>
2018-08-21 22:28:05 +0000

    refactor of classes, adding a lot of functions to __init__ to simplify

--- src/route_config.py
+++ src/route_config.py
@@ -1,9 +1,11 @@
+# this is where the routes are configured for the homepage, and the dashboard
 # 5 and only 5, 6th is TBD
 
-routesdict={
-    'route1': {'route':'86', 'grade':'A', 'description':'This description is now configured through route_config.py.','schedule_url':'https://www.njtransit.com/pdf/bus/T0086.pdf'},
-    'route2': {'route':'86P', 'grade':'B', 'description':'Serving Union City and the Newport Mall, this is a bus route for the ages.','schedule_url':'https://www.njtransit.com/pdf/bus/T0086.pdf'},
-    'route3': {'route':'87', 'grade':'C', 'description':'Serving Union City and the Newport Mall, this is a bus route for the ages.','schedule_url':'https://www.njtransit.com/pdf/bus/T0086.pdf'},
-    'route4': {'route':'119', 'grade':'D', 'description':'Serving Union City and the Newport Mall, this is a bus route for the ages.','schedule_url':'https://www.njtransit.com/pdf/bus/T0086.pdf'},
-    'route5': {'route':'123', 'grade':'A-', 'description':'Serving Union City and the Newport Mall, this is a bus route for the ages.','schedule_url':'https://www.njtransit.com/pdf/bus/T0086.pdf'}
-}
+
+reportcard_routes=[ \
+    {'route':'84', 'grade':'A', 'description':'Through-running DINGO from Journal Square to Union City along Palisade Ave every 15-20 minutes throughout the day.','schedule_url':'https://www.njtransit.com/pdf/bus/T0084.pdf'},
+    {'route':'86', 'grade':'B', 'description':'Our community\'s bus connection to downtown Jersey City\'s Grove Street area and the Newport Center Mall.','schedule_url':'https://www.njtransit.com/pdf/bus/T0086.pdf'},
+    {'route':'87', 'grade':'C', 'description':'The Heights\' main bus backbone, providing its most frequent links to rail transit at Journal Square and Hoboken Terminal.','schedule_url':'https://www.njtransit.com/pdf/bus/T0087.pdf'},
+    {'route':'119', 'grade':'D', 'description':'Our Central Avenue local and main express commuter line to midtown Manhattan.','schedule_url':'https://www.njtransit.com/pdf/bus/T0119.pdf'},
+    {'route':'123', 'grade':'A-', 'description':'Alternate Palisade Ave local service to Union City and Manhattan.','schedule_url':'https://www.njtransit.com/pdf/bus/T0123.pdf'}\
+    ]
