90bc3cb40d3088e88385467bfa728001f9d58e80
Anthony Townsend <anthony@bitsandatoms.net>
2018-09-22 20:33:31 -0400

    fixed import path in routewatcher

--- reportcard/routewatcher.py
+++ reportcard/routewatcher.py
@@ -5,8 +5,8 @@ import sys
 import argparse
 import datetime
 
-import reportcard.lib.BusAPI as BusAPI
-import reportcard.lib.BusRouteLogsDB as BusRouteLogsDB
+import lib.BusAPI as BusAPI
+import lib.BusRouteLogsDB as BusRouteLogsDB
 
 def db_setup(route):
     db = BusRouteLogsDB.MySQL('buses', 'buswatcher', 'njtransit', '127.0.0.1', route)
