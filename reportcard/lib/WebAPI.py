6671702edecf439761b294212e238e8a01e64724
Anthony Townsend <anthony@bitsandatoms.net>
2018-10-04 19:50:43 -0400

    API query parsing works for all routelog columns; pretty geoJSON output
    Former-commit-id: a1f191a800674efba004a554a0e7966808d4518a

--- reportcard/lib/WebAPI.py
+++ reportcard/lib/WebAPI.py
@@ -24,18 +24,12 @@ def data2geojson(df):
             , axis=1)
 
 
-    return geojson.dumps(geojson.FeatureCollection(features), indent=4, sort_keys=True, default=str)
-
-def gen_query(route,period,**kwargs):
-
+    # return geojson.dumps(geojson.FeatureCollection(features), indent=4, sort_keys=True, default=str)
+    return geojson.FeatureCollection(features)
 
 
 
-    return query, table_name
-
-
-
-def get_positions(route, period, **kwargs):
+def get_positions_byurl(route, period, **kwargs):
 
     # database initialization
     db = BusRouteLogsDB.MySQL('buses', 'buswatcher', 'njtransit', db_server, route)
@@ -56,7 +50,8 @@ def get_positions(route, period, **kwargs):
             table_name, route))
     elif period == "history":
         query = ('SELECT * FROM %s WHERE rt=%s ORDER BY timestamp DESC;' % (table_name, route))
-    else:
+    # elif period like "2018-08-10":
+        # query = ('SELECT * FROM %s WHERE (rt=%s AND DATE(`timestamp`)=("2018-08-10") ORDER BY timestamp DESC;' % (table_name, route))
         raise RuntimeError('Bad request sucker!')
 
     # if v is not None:
@@ -70,6 +65,63 @@ def get_positions(route, period, **kwargs):
     positions_geojson = data2geojson(positions_log)
 
     return positions_geojson
+
+
+def get_positions_byargs(args):
+
+    # database initialization
+    db = BusRouteLogsDB.MySQL('buses', 'buswatcher', 'njtransit', db_server, args['rt'])
+    conn = db.conn
+
+    table_name = 'routelog_' + args['rt']
+
+
+    sql_insert=str()
+    for key, value in args.items():
+        if key == 'period':
+            pass
+        else:
+            _sql_snippet = (" AND {}={}").format(key,value)
+            sql_insert+=_sql_snippet
+    # todo regexp find and delete first instance of " AND "
+    sql_insert=("("+sql_insert+")")
+
+
+    try:
+        if args['period'] == "daily":
+            query = ('SELECT * FROM %s WHERE (%s AND DATE(`timestamp`)=CURDATE() ) ORDER BY timestamp DESC;' % (
+                table_name, sql_insert))
+        elif args['period']  == "yesterday":
+            query = (
+                    'SELECT * FROM %s WHERE (%s AND (timestamp >= CURDATE() - INTERVAL 1 DAY AND timestamp < CURDATE())) ORDER BY timestamp DESC;' % (
+                table_name, sql_insert))
+        elif args['period']  == "weekly":
+            query = (
+                    'SELECT * FROM %s WHERE (%s AND (YEARWEEK(`timestamp`, 1) = YEARWEEK(CURDATE(), 1))) ORDER BY timestamp DESC;' % (
+                table_name, sql_insert))
+        elif args['period']  == "history":
+            query = ('SELECT * FROM %s WHERE %s ORDER BY timestamp DESC;' % (table_name, sql_insert))
+        # elif kwargs['period']  like "2018-08-10":
+            # query = ('SELECT * FROM %s WHERE (%s AND DATE(`timestamp`)=("2018-08-10") ORDER BY timestamp DESC;' % (table_name, sql_insert))
+            raise RuntimeError('Bad request sucker!')
+
+    except:
+        # no period defined, fetch entire history
+
+        query = ('SELECT * FROM %s WHERE %s ORDER BY timestamp DESC;' % (table_name, sql_insert))
+
+    # remove the leading AND in query
+    query = query.replace(' AND ', '',1)
+
+    # get data and basic cleanup
+    positions_log = pd.read_sql_query(query, conn)
+    # df_temp = df_temp.drop(columns=['cars', 'consist', 'fd', 'm', 'name', 'rn', 'scheduled'])
+    positions_log = timestamp_fix(positions_log)
+
+    positions_geojson = data2geojson(positions_log)
+
+    return positions_geojson
+
 
 def get_arrivals(route,stop,period, args):
     # if v not None:
