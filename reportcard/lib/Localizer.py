8daad18ff54719d96d97940d3f4c2240e74ac5e5
Anthony Townsend <anthony@bitsandatoms.net>
2018-11-21 20:10:21 -0500

    1st commit
    Former-commit-id: b0d6c40a9ec47586c99d0764483df0c7303dab07

---
+++ reportcard/lib/Localizer.py
@@ -0,0 +1,16 @@
+import scipy.spatial as sp
+
+def infer_stops(bus_data, stopslist): # called from routewatcher.py
+    # takes a bus_data object
+    # and a stopslist from lib.BusAPI.parse_xml_getRoutePoints
+
+    sp.whatever
+
+    # log the results
+    bus_data.stop_id =
+    bus_data.closest_approach_distance
+    bus_data.duration_in_proximity_to_stop
+
+    # and return
+
+    return bus_data
