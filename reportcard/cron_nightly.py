83e45ec2ca943cb4217eddd73b529c00b0e33d24
anthony townsend <anthony@bitsandatoms.net>
2018-12-08 18:46:06 -0500

    caching most views and pickling of cron bunching reports done
    Former-commit-id: 5e96bb4871e66f9e6b8583d4ce48ec4a5953c1e8

--- reportcard/cron_nightly.py
+++ reportcard/cron_nightly.py
@@ -1,6 +1,4 @@
 import lib.ReportCard as ReportCard
-import lib.BusAPI as BusAPI
-import lib.WebAPI as WebAPI
 
 from route_config import reportcard_routes,grade_descriptions
 
@@ -17,11 +15,12 @@ for rt_no in reportcard_routes:
     # create base RouteReport instance
     routereport=ReportCard.RouteReport(source,rt_no['route'],reportcard_routes,grade_descriptions)
 
-    # generate individual reports
+    # generate individual reports to a pickle file
+
     # every report in lib.ReportCard should have an easy_cache decorator or else we are wasting time
 
     # generate bunching leaderboard
-    routereport.get_bunching_leaderboard(route=rt_no['route'],period=period)
+    routereport.generate_bunching_leaderboard(route=rt_no['route'],period=period)
 
     # generate other reports
     # routereport.get_bunching_leaderboard()
