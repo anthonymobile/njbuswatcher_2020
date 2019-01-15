8ad84af0710bc0af094d6928540a7ef5e436afb0
anthony townsend <anthony@bitsandatoms.net>
2019-01-14 19:25:28 -0500

    WIP - need to rewrite a single polyfit classifier for all approaches with more than 2 positions, and another for any with just 2 (any with 1 are still held or automatically assigned already)
    Former-commit-id: a6abf4a38c0783571e2c3ee666264b15ecdade5d

--- reportcard/tripwatcher.py
+++ reportcard/tripwatcher.py
@@ -9,8 +9,9 @@ parser.add_argument('-r', '--route', dest='route', required=True, help='route nu
 args = parser.parse_args()
 
 from lib import BusAPI, Localizer
-
 from lib import DataBases as db
+
+import scipy
 
 import time
 while True:
@@ -148,73 +149,90 @@ while True:
                     print (('\t\t {a:.0f} distance_to_stop {b}').format(a=point[0], b=point[1]))
 
 
-                # todo NEW APPROACH CLASSIFER
-
-                # 1) fit the points to a line (current Case D method), or if n>2 a polyline
-                # 2) get the min of that function
-                # 3) find the point in the array closest to the min
 
 
-                # old version not working - for instance, since this has avg_slope that's negative, it becomes a Case B and never gets to Case D
+                ################################################33
+                #
+                #
+                ######## DEBUGGING
 
+                UNASSIGNED
+                TRIPS
+
+                analyzing
+                arrival
+                candidates
+                on
+                trip
+                5739_15_20190114...
+
+                # these 3 suggests --- test for # of positions is not good for filtering
+                # polyfit these also....?
+                # or just do a simple slope?
+                # is there one approach for anything over 2 positions that can filter B,C from D?
                 #             approaching
-                #             20692
+                #             20610
                 #             0
                 #             distance_to_stop
-                #             239.257462834839
+                #             210.16058526919383
                 #             1
                 #             distance_to_stop
-                #             1.0971433864275721
+                #             210.16058526919383
                 #             2
                 #             distance_to_stop
-                #             209.27792840019302
-                # caseB
+                #             274.4702380028972
+                #
+                #         approaching
+                #         20496
+                #         0
+                #         distance_to_stop
+                #         1.5360106211100863
+                #         1
+                #         distance_to_stop
+                #         1.5360106211100863
+                #         2
+                #         distance_to_stop
+                #         1.5360106211100863
+                #         3
+                #         distance_to_stop
+                #         1.5360106211100863
+                #         4
+                #         distance_to_stop
+                #         1.5360106211100863
+                #         5
+                #         distance_to_stop
+                #         196.9567041019691
+                #
+                #     approaching
+                #     31731
+                #     0
+                #     distance_to_stop
+                #     260.09398414587315
+                #     1
+                #     distance_to_stop
+                #     318.33337343873944
+                #     2
+                #     distance_to_stop
+                #     318.33337343873944
+                #     3
+                #     distance_to_stop
+                #     616.3932991517729
+                #
+                # # this one suggests +/- 10 percent error on the distance to stop?
+                # approaching
+                # 21066
+                # 0
+                # distance_to_stop
+                # 588.4626107067455
+                # 1
+                # distance_to_stop
+                # 595.9485389675923
+                # 2
+                # distance_to_stop
+                # 62.383752097023596
 
 
-                # 2 calculate classification metrics
-
-                slope = np.diff(approach_array, axis=0)[:, 1]
-                acceleration = np.diff(slope, axis=0)
-                slope_avg=np.mean(slope, axis=0)
-
-
-                # 3 filter by case
-
-
-
-                # CASE A sitting at the stop, then gone without a trace
-                # determined by [d is <100, doesn't change e.g. slope = 0 ]
-                # (0, 50) ***
-                # (1, 50)
-                # (2, 50)
-                # (3, 50)
-                if slope_avg == 0:
-                    arrival_time = position_list[0].distance_to_stop
-                    case_frequencies['caseA'] += 1
-                    print ('caseA')
-
-                # CASE B approaches, then vanishes
-                # determined by [d is decreasing, slope is always negative]
-                # (0, 400)
-                # (1, 300)
-                # (2, 200)
-                # (3, 50) ***
-                elif slope_avg < 0:
-                    arrival_time = position_list[-1].distance_to_stop
-                    case_frequencies['caseB'] += 1
-                    print('caseB')
-
-                # CASE C appears, then departs
-                # determined by [d is increasing, slope is always positive]
-                # (0, 50) ***
-                # (1, 100)
-                # (2, 200)
-                # (3, 300)
-                elif (slope_avg > 0):
-                    arrival_time = position_list[0].distance_to_stop
-                    case_frequencies['caseC'] += 1
-                    print('caseC')
-
+                # if its a case D
                 # CASE D approach, stop, depart
                 # determined by [d is decreasing, slope is negative, then inverts and d is decreasing, slope is increasing, assign to point of lowest d]
                 # (0, 200)
@@ -224,41 +242,116 @@ while True:
                 # ASSIGNMENT
                 # arrival_time = (1)
 
-                elif len(position_list) > 1:
-
+                if len(position_list) > 2:
                     # polyfit the line
+                    try:
 
-                    try: # quadratic
                         z_quad = np.polyfit(approach_array[:, 0], approach_array[:, 1], 2)
-                        print(('quad {a}x2 + {b}x + {c}').format(a=z_quad[0], b=z_quad[1], c=z_quad[2]))
-                    except: # line
-                        z_line = np.polyfit(approach_array[:, 0], approach_array[:, 1], 1)
-                        print(('line {a}x + {b}').format(a=z_line[0], b=z_line[1]))
 
+                        # if the coefficient on x2 is positive, we are concave up and this is definitely a CASE D:
+                        if z_quad[0] > 0:
 
+                            # https://stackoverflow.com/questions/29634217/get-minimum-points-of-numpy-poly1d-curve
+                            c = np.poly1d(z_quad)
+                            crit = c.deriv().r
+                            r_crit = crit[crit.imag == 0].real
+                            test = c.deriv(2)(r_crit)
 
-                    # if the coefficient on x2 is positive, we are concave up and this is definitely a CASE D:
-                    if z.quad[0] > 0:
-                        case_frequencies['caseD'] += 1
+                            # compute local minima
+                            # excluding range boundaries
+                            x_min = r_crit[test > 0]
+                            y_min = c(x_min)
 
-                        # now lets find the minimum of the fitted curve
-                        # using https://nagordon.github.io/mechpy/Curve_fitting_and_Optimization_with_python.html
+                            arrival_position = int(round(x_min))-1 # round to nearest position in the approach_array
+                            # todo for position in approach_array: check that this is indeed the minimum
 
-                        y  = np.poly1d(z_quad)
-                        xguess = 1 # per above chart todo need to figure how to iterate here?
-                        y_min = scipy.optimize.minimize(y, xguess)
+                            arrival_time = position_list[arrival_position].timestamp
+                            print ('caseD {a}'.format(a=arrival_time))
+                            case_frequencies['caseD'] += 1
 
-                        # and then figure out which value element in the approach_array it is closest to
-                        # closest to approach_array[:, 0]
+                    except:
+                        pass
 
-                        print('caseD')
+                # just do the working case A,B,C filter
+                else:
 
+                    # old fit to line
+                    # z_line = np.polyfit(approach_array[:, 0], approach_array[:, 1], 1)
 
+                    # calculate classification metrics
 
-                # CASE E boomerang
-                # already arrived at this stop on this trip and now passing by again, this is closest stop on another leg
-                # (e.g. 87 going down the hill after palisade)
-                # these OUGHT to be filtered out by the 'arrival_flags'
+                    slope = np.diff(approach_array, axis=0)[:, 1]
+                    acceleration = np.diff(slope, axis=0)
+                    slope_avg=np.mean(slope, axis=0)
+
+                    # CASE A sitting at the stop, then gone without a trace
+                    # determined by [d is <100, doesn't change e.g. slope = 0 ]
+                    # (0, 50) ***
+                    # (1, 50)
+                    # (2, 50)
+                    # (3, 50)
+                    if slope_avg == 0:
+                        arrival_time = position_list[0].timestamp
+                        case_frequencies['caseA'] += 1
+                        print ('caseA {a}'.format(a=arrival_time))
+
+                    # CASE B approaches, then vanishes
+                    # determined by [d is decreasing, slope is always negative]
+                    # (0, 400)
+                    # (1, 300)
+                    # (2, 200)
+                    # (3, 50) ***
+                    elif slope_avg < 0:
+                        arrival_time = position_list[-1].timestamp
+                        case_frequencies['caseB'] += 1
+                        print ('caseB {a}'.format(a=arrival_time))
+
+                    # CASE C appears, then departs
+                    # determined by [d is increasing, slope is always positive]
+                    # (0, 50) ***
+                    # (1, 100)
+                    # (2, 200)
+                    # (3, 300)
+                    elif (slope_avg > 0):
+                        arrival_time = position_list[0].timestamp
+                        case_frequencies['caseC'] += 1
+                        print ('caseC {a}'.format(a=arrival_time))
+
+                    # CASE E boomerang
+                    # already arrived at this stop on this trip and now passing by again, this is closest stop on another leg
+                    # (e.g. 87 going down the hill after palisade)
+                    # these OUGHT to be filtered out by the 'arrival_flags'
+
+                    # OLD CASE D
+                    #
+                    # elif len(position_list) > 1:
+                    #
+                    #     # polyfit the line
+                    #
+                    #     try: # quadratic
+                    #         z_quad = np.polyfit(approach_array[:, 0], approach_array[:, 1], 2)
+                    #         print(('quad {a}x2 + {b}x + {c}').format(a=z_quad[0], b=z_quad[1], c=z_quad[2]))
+                    #     except: # line
+                    #         z_line = np.polyfit(approach_array[:, 0], approach_array[:, 1], 1)
+                    #         print(('line {a}x + {b}').format(a=z_line[0], b=z_line[1]))
+                    #
+                    #
+                    #
+                    #     # if the coefficient on x2 is positive, we are concave up and this is definitely a CASE D:
+                    #     if z.quad[0] > 0:
+                    #         case_frequencies['caseD'] += 1
+                    #
+                    #         # now lets find the minimum of the fitted curve
+                    #         # using https://nagordon.github.io/mechpy/Curve_fitting_and_Optimization_with_python.html
+                    #
+                    #         y  = np.poly1d(z_quad)
+                    #         xguess = 1 # per above chart todo need to figure how to iterate here?
+                    #         y_min = scipy.optimize.minimize(y, xguess)
+                    #
+                    #         # and then figure out which value element in the approach_array it is closest to
+                    #         # closest to approach_array[:, 0]
+                    #
+                    #         print('caseD')
 
 
         print (case_frequencies)
@@ -267,11 +360,8 @@ while True:
         session.commit() # sends any updates made above (like setting the arrival flag
 
         # 4
-
         # update the database
         # get a session
-
-
         #
         # add position_update to the session as table update
         # log the arrival_timestamp for corresponding ScheduledStop
