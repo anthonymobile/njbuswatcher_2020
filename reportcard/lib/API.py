cbd4c3418349fa200146daac19d1a70b4f60d0b5
anthony townsend <anthony@bitsandatoms.net>
2019-02-03 09:38:18 -0500

    API works! but filters borked still... results are empty

--- reportcard/lib/API.py
+++ reportcard/lib/API.py
@@ -106,7 +106,7 @@ def get_positions_byargs(args):
 
 
             # cleanup
-            positions_log = positions_log.drop(columns=['cars', 'consist', 'm','pdRtpiFeedName','rt','rtRtpiFeedName','rtdd','wid1','wid2'])
+            positions_log = positions_log.drop(columns=['cars', 'consist', 'm','pdrtpifeedname','rt','rtrtpifeedname','rtdd','wid1','wid2'])
             positions_log = timestamp_fix(positions_log)
             positions_geojson = positions2geojson(positions_log)
 
