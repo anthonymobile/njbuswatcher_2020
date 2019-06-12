0295a5c90ac905d35afa80b8f619e5f161379a31
Anthony Townsend <anthony@bitsandatoms.net>
2019-06-12 16:37:28 -0400

    added @check_maintenance decorator for flask routes to trigger regular updates to route definitions, bunching reports, etc.
    Former-commit-id: b3b59ebcda96ea0390424db1a3eb56d55f880e8b

--- buswatcher/tripwatcher.py
+++ buswatcher/tripwatcher.py
@@ -1,9 +1,8 @@
 #
 # usage:
-# (statewide)                           tripwatcher.py
-# (only routes in route_config.py)      tripwatcher.py --limit
+# (statewide)                                           tripwatcher.py
+# (only routes in config/route_description.json)        tripwatcher.py --limit
 #
-
 
 import argparse
 import sys
@@ -15,6 +14,102 @@ import numpy as np
 from buswatcher.lib import BusAPI, Localizer
 from buswatcher.lib.DataBases import SQLAlchemyDBConnection, Trip, BusPosition, ScheduledStop
 from buswatcher.lib.RouteConfig import load_config
+
+
+
+def maintenance():
+
+    # a decorator
+
+
+    # update route_definitons.json
+
+        # load the route_definitons.json
+        # check the ttl
+
+
+    # todo 1 dump this into a decorator in www.py so it runs everytime a page is triggered, and it checks the TTL on the config/route_descriptions.json file
+
+    # from othermodule import decorator
+    #
+    # @decorator
+    # class Someclass:
+    #     pass
+
+
+    import buswatcher.lib.wwwAPI as wwwAPI
+    import buswatcher.lib.RouteConfig as RouteConfig
+
+    # hardcode transit system
+    source = 'nj'
+
+    # hardcode period
+    period = 'weekly'
+
+    # loop over all routes
+    for rt_no in reportcard_routes: # todo 3 fix this
+
+        # create base RouteReport instance
+        routereport=wwwAPI.RouteReport(source,rt_no['route'])
+
+        # generate individual reports to a pickle file
+
+        # generate bunching leaderboard
+        routereport.generate_bunching_leaderboard(route=rt_no['route'],period=period)
+
+        # generate other reports
+        # e.g. routereport.get_bunching_leaderboard()
+
+
+    # todo RouteConfig.fetch_update_route_metadata
+
+    # check ttl then run it
+
+
+
+
+
+
+###################################################
+#  old cron_nightly.py
+#  generates bunching reports
+###################################################
+
+# import lib.ReportCard as ReportCard
+#
+# from route_config import reportcard_routes,grade_descriptions
+#
+#
+# # hardcode transit system
+# source = 'nj'
+#
+# # hardcode period
+# period = 'weekly'
+#
+# # loop over all routes
+# for rt_no in reportcard_routes:
+#
+#     # create base RouteReport instance
+#     routereport=ReportCard.RouteReport(source,rt_no['route'],reportcard_routes,grade_descriptions)
+#
+#     # generate individual reports to a pickle file
+#
+#     # every report in lib.ReportCard should have an easy_cache decorator or else we are wasting time
+#
+#     # generate bunching leaderboard
+#     routereport.generate_bunching_leaderboard(route=rt_no['route'],period=period)
+#
+#     # generate other reports
+#     # routereport.get_bunching_leaderboard()
+#
+# a
+
+
+
+
+
+
+
 
 
 def watch(limit,r):
