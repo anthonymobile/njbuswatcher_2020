aea7789bdc06097a0c4d9ae3df505fd782276129
Anthony Townsend <amt3@nyu.edu>
2020-03-08 11:56:46 -0400

    added bootstrap template
    Former-commit-id: 92d1d13c64c2deb79ffb1923234da64972261631

--- buswatcher/www2.py
+++ buswatcher/www2.py
@@ -1,91 +1,91 @@
 # bus buswatcher v3.0
 # todo rebuild the app a single-page app that updates based on your choices
-
 # mar 2020 - by anthony@starcitygroup.us
-
-import pandas as pd
-import pymysql as db
-from sqlalchemy import create_engine
-
-# fetch data
-def get_arrivals_by_hour(route):
-
-    # database variables
-    user = 'buswatcher'
-    password = 'njtransit'
-    database = 'buses'
-
-    db_connection_str = 'mysql+pymysql://{}:{}@localhost/{}'.format(user, password, database)
-    engine = create_engine(db_connection_str)
-
-    conn = connection = engine.connect()
-
-    q = """
-        SELECT scheduledstop_log.trip_id, trip_log.trip_id, scheduledstop_log.date, trip_log.rt, \
-               scheduledstop_log.run, trip_log.pd, scheduledstop_log.v,  scheduledstop_log.stop_id, \
-               scheduledstop_log.stop_name, scheduledstop_log.lat, scheduledstop_log.lon, \
-               scheduledstop_log.arrival_timestamp
-          FROM scheduledstop_log,trip_log
-         WHERE scheduledstop_log.trip_id = trip_log.trip_id
-           AND arrival_timestamp is not null 
-           AND trip_log.rt = {};
-    """.format(route)
-
-    df = pd.read_sql_query(q, conn)
-    df['hour_of_arrival_timestamp'] = df['arrival_timestamp'].dt.hour
-
-    return df.groupby(['hour_of_arrival_timestamp']).size().to_frame(name = 'arrivals')
 
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+import lib.DashHelpers as dh
+
 from lib.TransitSystem import load_system_map
 
 
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+######################################### my logic
 
 route=27
-df_arrivals_by_hour=get_arrivals_by_hour(route)
-
-app.layout = html.Div(children=[
-    html.H1(children='NJBusWatcher'),
-
-    html.Div(children='''
-         Residents and businesses depend on NJTransit buses every day. But its hard to evaluate the quality of bus service. That's why we built this site to provide a one-stop shop for bus performance information. Here you can see data on past performance and view maps of current service. 
-    '''),
+df_arrivals_by_hour=dh.get_arrivals_by_hour(route)
 
 
-    dcc.Graph(id='arrival-histogram',
-              figure={
-                'data': [
-                    {'x': df_arrivals_by_hour.index, 'y': df_arrivals_by_hour['arrivals'], 'type': 'bar', 'name': 'Arrivals'}
-                  ],
-                'layout': {
-                      'title': 'Arrivals By Hour For A Route'
-                  }
-              }
-
-    ),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+######################################### page template (dash bootstrap)
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Link", href="#")),
+        dbc.DropdownMenu(
+            nav=True,
+            in_navbar=True,
+            label="Menu",
+            children=[
+                dbc.DropdownMenuItem("Entry 1"),
+                dbc.DropdownMenuItem("Entry 2"),
+                dbc.DropdownMenuItem(divider=True),
+                dbc.DropdownMenuItem("Entry 3"),
             ],
-            'layout': {
-                'title': 'Arrivals By Hour For A Route'
-            }
-        }
-    )
-]
+        ),
+    ],
+    brand="Demo",
+    brand_href="#",
+    sticky="top",
 )
+
+body = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.H2("NJBusWatcher"),
+                        html.P(
+                            """\
+Residents and businesses depend on NJTransit buses every day. But its hard to evaluate the quality of bus service. \
+That's why we built this site to provide a one-stop shop for bus performance information. \
+Here you can see data on past performance and view maps of current service."""
+                        ),
+                        dbc.Button("View details", color="secondary"),
+                    ],
+                    md=4,
+                ),
+                dbc.Col(
+                    [
+                        html.H2("Graph"),
+                        dcc.Graph(id='arrival-histogram',
+                                  figure={
+                                      'data': [
+                                          {'x': df_arrivals_by_hour.index, 'y': df_arrivals_by_hour['arrivals'],
+                                           'type': 'bar', 'name': 'Arrivals'}
+                                      ],
+                                      'layout': {
+                                          'title': 'Arrivals By Hour For A Route'
+                                      }
+                                  }
+                                  ),
+                    ]
+                ),
+            ]
+        )
+    ],
+    className="mt-4",
+)
+
+
+
+######################################### app logic
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
 # todo add a callback with a dropdown to pick the route
+app.layout = html.Div([navbar, body])
+
 
 
 
@@ -96,11 +96,6 @@ app.layout = html.Div(children=[
 if __name__ == '__main__':
     system_map = load_system_map()
     app.run_server(debug=True)
-
-# if __name__ == "__main__":
-#     system_map=load_system_map()
-#     app.run(host='0.0.0.0', debug=True)
-
 
 # # after https://medium.com/@trstringer/logging-flask-and-gunicorn-the-manageable-way-2e6f0b8beb2f
 # if __name__ != "__main__":
