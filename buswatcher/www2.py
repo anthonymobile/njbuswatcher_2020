66ec6b006219cd39a272c6f67c7a0c3518071038
Anthony Townsend <amt3@nyu.edu>
2020-03-07 21:40:01 -0500

    first chart working
    Former-commit-id: 22ef3caa9a4fdc53226e7e4f20d0e5e16db000d5

--- buswatcher/www2.py
+++ buswatcher/www2.py
@@ -3,22 +3,73 @@
 
 # mar 2020 - by anthony@starcitygroup.us
 
-# -*- coding: utf-8 -*-
+import pandas as pd
+import pymysql as db
+from sqlalchemy import create_engine
+
+# fetch data
+def get_arrivals_by_hour(route):
+
+    # database variables
+    user = 'buswatcher'
+    password = 'njtransit'
+    database = 'buses'
+
+    db_connection_str = 'mysql+pymysql://{}:{}@localhost/{}'.format(user, password, database)
+    engine = create_engine(db_connection_str)
+
+    conn = connection = engine.connect()
+
+    q = """
+        SELECT scheduledstop_log.trip_id, trip_log.trip_id, scheduledstop_log.date, trip_log.rt, \
+               scheduledstop_log.run, trip_log.pd, scheduledstop_log.v,  scheduledstop_log.stop_id, \
+               scheduledstop_log.stop_name, scheduledstop_log.lat, scheduledstop_log.lon, \
+               scheduledstop_log.arrival_timestamp
+          FROM scheduledstop_log,trip_log
+         WHERE scheduledstop_log.trip_id = trip_log.trip_id
+           AND arrival_timestamp is not null 
+           AND trip_log.rt = {};
+    """.format(route)
+
+    df = pd.read_sql_query(q, conn)
+    df['hour_of_arrival_timestamp'] = df['arrival_timestamp'].dt.hour
+
+    return df.groupby(['hour_of_arrival_timestamp']).size().to_frame(name = 'arrivals')
+
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from lib.TransitSystem import load_system_map
 
+
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+route=27
+df_arrivals_by_hour=get_arrivals_by_hour(route)
+
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='NJBusWatcher'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+         Residents and businesses depend on NJTransit buses every day. But its hard to evaluate the quality of bus service. That's why we built this site to provide a one-stop shop for bus performance information. Here you can see data on past performance and view maps of current service. 
     '''),
+
+
+    dcc.Graph(id='arrival-histogram',
+              figure={
+                'data': [
+                    {'x': df_arrivals_by_hour.index, 'y': df_arrivals_by_hour['arrivals'], 'type': 'bar', 'name': 'Arrivals'}
+                  ],
+                'layout': {
+                      'title': 'Arrivals By Hour For A Route'
+                  }
+              }
+
+    ),
 
     dcc.Graph(
         id='example-graph',
@@ -28,13 +79,13 @@ app.layout = html.Div(children=[
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'title': 'Arrivals By Hour For A Route'
             }
         }
     )
-])
-
-# todo https://dash.plot.ly/urls Multi-Page Apps and URL Support
+]
+)
+# todo add a callback with a dropdown to pick the route
 
 
 
@@ -51,9 +102,9 @@ if __name__ == '__main__':
 #     app.run(host='0.0.0.0', debug=True)
 
 
-# after https://medium.com/@trstringer/logging-flask-and-gunicorn-the-manageable-way-2e6f0b8beb2f
-if __name__ != "__main__":
-    system_map = load_system_map()
-    gunicorn_logger = logging.getLogger("gunicorn.error")
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
+# # after https://medium.com/@trstringer/logging-flask-and-gunicorn-the-manageable-way-2e6f0b8beb2f
+# if __name__ != "__main__":
+#     system_map = load_system_map()
+#     gunicorn_logger = logging.getLogger("gunicorn.error")
+#     app.logger.handlers = gunicorn_logger.handlers
+#     app.logger.setLevel(gunicorn_logger.level)
