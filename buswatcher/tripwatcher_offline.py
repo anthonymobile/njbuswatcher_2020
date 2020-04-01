4e15d9994afaf1f37a70a5299ad670fbcee271e8
Anthony Townsend <amt3@nyu.edu>
2020-04-01 17:17:36 -0400

    gave up on database migration
    Former-commit-id: 111ecf351d9b182efa30bf2281277baa3601043f

--- buswatcher/tripwatcher_offline.py
+++
@@ -1,9 +0,0 @@
-# tool for working with offline archived data
-# todo data needs to be migrated to new db format first
-
-from lib.TransitSystem import load_system_map
-from lib.BusProcessor import BusProcessor
-
-system_map = load_system_map()
-BusProcessor(system_map, mode='offline')
-
